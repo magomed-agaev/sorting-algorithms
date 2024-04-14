a19cba76a9cba7ba4a92efd68667000632005351
magaev <magomed.agaev@laplateforme.io>
2024-04-15 00:23:32 +0200

    tri par selection bien modifie et  realise

--- selection.py
+++ selection.py
@@ -1,17 +1,22 @@
-a = [-3, 5, 0, -8, 1, 10]
-N = len(a)
-#
-for i in range(N-1):
-    m = a[i]
-    p = i
-    for j in range(i+1, N):
-        if m > a[j]:
-            m = a[j]
-            p = j
+liste = [4, 13, 11, 6, 9, 7, 10, 12, 1, 5, 15, 3, 8, 14, 2]
 
-    if p != i:
-        t = a[i]
-        a[i] = a[p]
-        a[p] = t
 
-    print(a)
+def tri_selection(liste):
+    lenght = len(liste)
+    while lenght != 0:
+        for i in range(lenght):
+            selection = [0]
+            for j in range(lenght):
+                if selection[0] < liste[j]:
+                    selection = [liste[j]]
+                    index = j
+
+            lenght -= 1
+            liste[index] = liste[lenght]
+            liste[lenght] = selection[0]
+
+            selection = [liste[0]]
+    print(liste)
+
+
+tri_selection(liste)
