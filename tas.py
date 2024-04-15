1a60b6eecd41c31a0585f81127b28844f91fbb40
mansour-agaev <mansour.agaev@laplateforme.io>
2024-04-15 14:48:17 +0200

    fichier suprimé

--- tas.py
+++ tas.py
@@ -26,7 +26,6 @@ def tri_par_tas(tableau):
     return tableau
 
 
-# Exemple d'utilisation
 liste_a_trier = [3, 7, 2, 1, 9, 5]
 print("Avant le tri :", liste_a_trier)
 liste_triee = tri_par_tas(liste_a_trier)
